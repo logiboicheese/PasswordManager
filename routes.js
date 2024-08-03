5abe9312b82958dc06e78c2fff945ae6a936732e
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-08-03 20:53:23 +0200

    clean code

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const passwordController = require('./passwordController');
+
 const router = express.Router();
+
 router.post('/passwords', passwordController.createPassword);
 router.put('/passwords/:id', passwordController.updatePassword);
 router.delete('/passwords/:id', passwordController.deletePassword);
 router.get('/passwords', passwordController.getPasswords);
+
 module.exports = router;
