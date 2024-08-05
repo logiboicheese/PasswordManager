0ae7b9e6fd4f0009ffc7ebf5d86dfc037532a3fa
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-08-05 02:19:49 +0200

    error handling update

--- routes.js
+++ routes.js
@@ -3,9 +3,18 @@ const passwordController = require('./passwordController');
 
 const router = express.Router();
 
-router.post('/passwords', passwordController.createPassword);
-router.put('/passwords/:id', passwordController.updatePassword);
-router.delete('/passwords/:id', passwordController.deletePassword);
-router.get('/passwords', passwordController.getPasswords);
+const asyncErrorHandler = fn => 
+    (req, res, next) => 
+        Promise.resolve(fn(req, res, next)).catch(next);
+
+router.post('/passwords', asyncErrorHandler(passwordController.createPassword));
+router.put('/passwords/:id', asyncErrorHandler(passwordController.updatePassword));
+router.delete('/passwords/:id', asyncErrorHandler(passwordController.deletePassword));
+router.get('/passwords', asyncErrorHandler(passwordController.getPasswords));
+
+router.use((err, req, res, next) => {
+    console.error(err); 
+    res.status(500).send({ error: 'An internal server error has occurred.' });
+});
 
 module.exports = router;
