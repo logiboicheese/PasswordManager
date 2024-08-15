e746c851307dc4ba159f276c0d793fe4e7cc8d22
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-08-15 12:20:56 +0200

    implemented feature

--- passwordController.js
+++ passwordController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const bcrypt = require('bcrypt'); 
 const PasswordEntry = require('./passwordModel');
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -11,7 +12,11 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const createPasswordEntry = async (req, res) => {
     try {
-        const newPasswordEntry = new PasswordEntry(req.body);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newPasswordEntry = new PasswordEntry({
+            ...req.body,
+            password: hashedPassword, 
+        });
         await newPasswordEntry.save();
         res.status(201).json(newPasswordEntry);
     } catch (error) {
@@ -39,7 +44,12 @@ const getPasswordEntryById = async (req, res) => {
 
 const updatePasswordEntry = async (req, res) => {
     try {
-        const updatedPasswordEntry = await PasswordEntry.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); 
+        const passwordEntryUpdates = {
+            ...req.body,
+            password: hashedPassword,
+        };
+        const updatedPasswordEntry = await PasswordEntry.findByIdAndUpdate(req.params.id, passwordEntryUpdates, { new: true });
         res.status(200).json(updatedPasswordEntry);
     } catch (error) {
         res.status(400).json({ message: error.message });
