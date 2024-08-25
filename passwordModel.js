2b180e137bc17db957baed0c9fb1248b939e952a
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-08-25 03:21:53 +0200

    implemented function

--- passwordModel.js
+++ passwordModel.js
@@ -1,12 +1,49 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const Schema = mongoose.Schema;
+
 const passwordSchema = new Schema({
   service: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  dateCreated: { type: Date, default: Date.now }
+  dateCreated: { type: Date, default: Date.now },
 });
+
+passwordSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+passwordSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
 const Password = mongoose.model('Password', passwordSchema);
+
 module.exports = Password;
+```
+```javascript
+user.comparePassword(candidatePassword, function(err, isMatch) {
+  if (err) throw err;
+  console.log('Password Match:', isMatch);
+});
