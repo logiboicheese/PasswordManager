61e3d1641036014db4406dd216cfb8bd02b375c8
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-08-27 12:49:26 +0200

    implemented feature

--- passwordModel.js
+++ passwordModel.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
+const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR ? parseInt(process.env.SALT_WORK_FACTOR, 10) : 10;
 
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
+
+const auditLogSchema = new Schema({
+  action: { type: String, required: true },
+  username: { type: String, required: true },
+  service: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const AuditLog = mongoose.model('AuditLog', auditLogSchema);
 
 const passwordSchema = new Schema({
   service: { type: String, required: true },
@@ -16,6 +25,8 @@ const passwordSchema = new Schema({
 
 passwordSchema.pre('save', function(next) {
   const user = this;
+
+  AuditLog.create({ action: "CREATE", username: user.username, service: user.service });
 
   if (!user.isModified('password')) return next();
 
@@ -34,16 +45,14 @@ passwordSchema.pre('save', function(next) {
 passwordSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) return cb(err);
+
+    const action = isMatch ? "AUTH_SUCCESS" : "AUTH_FAILURE";
+    AuditLog.create({ action, username: this.username, service: this.service });
+
     cb(null, isMatch);
-  });
+  }.bind(this)); 
 };
 
 const Password = mongoose.model('Password', passwordSchema);
 
-module.exports = Password;
-```
-```javascript
-user.comparePassword(candidatePassword, function(err, isMatch) {
-  if (err) throw err;
-  console.log('Password Match:', isMatch);
-});
+module.exports = { Password, AuditLog };
