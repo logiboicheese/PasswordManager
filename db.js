598186c9f775a9d1627cd2c9e71ea791482bdf21
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-09-27 15:25:21 +0200

    cleaning code

--- db.js
+++ db.js
@@ -3,20 +3,11 @@ const mongoose = require('mongoose');
 
 const dbConnectionURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(dbConnectionURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        console.log('MongoDB is connected...');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit(1);
-    }
-};
-
-connectDB();
+mongoose.connect(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB is connected...'))
+  .catch(err => {
+      console.error('Failed to connect to MongoDB', err);
+      process.exit(1);
+  });
 
 module.exports = mongoose.connection;
