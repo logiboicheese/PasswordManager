fb0f70b2ccbe7d4052ae55f8cde85cd79c69c728
logiboicheese <92887273+logiboicheese@users.noreply.github.com>
2024-10-14 17:20:25 +0200

    implemented function

--- db.js
+++ db.js
@@ -1,13 +1,64 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const dbConnectionURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 mongoose.connect(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB is connected...'))
   .catch(err => {
-      console.error('Failed to connect to MongoDB', err);
-      process.exit(1);
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
   });
 
-module.exports = mongoose.connection;
+const PasswordEntrySchema = new mongoose.Schema({
+  website: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, {
+  timestamps: true,
+});
+
+PasswordEntrySchema.pre('save', function(next) {
+  let user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+PasswordEntrySchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+const PasswordEntry = mongoose.model('PasswordEntry', PasswordEntrySchema);
+
+module.exports = {
+  connection: mongoose.connection,
+  PasswordEntry,
+};
